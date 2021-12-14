02ce8a454ddded0d5685389c687319998996818d
ana-zrnic <ana.zrnic@student.pmf.unibl.org>
2021-12-14 22:51:43 +0100

    testiranje

--- src/test.java
+++ src/test.java
@@ -1,5 +1,11 @@
+import java.sql.*;
+
 public class test {
+    public void test(){
+    }
     public static void main(String[] args) {
+        Ucenik ucenik = new Ucenik("Jovan");
+        System.out.println(ucenik.getIme());
 
     }
 }
