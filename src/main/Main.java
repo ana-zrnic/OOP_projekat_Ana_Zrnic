3ab5e9f5871580d09b863032bd97f6bf1aa516ad
ana-zrnic <ana.zrnic@student.pmf.unibl.org>
2022-01-02 22:31:34 +0100

    uvezen javafx i working login page

---
+++ src/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import radSaBazom.dbConn;
+import radSaBazom.dbMetode;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("resources/login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 700);
+        stage.setTitle("E-dnevnik");
+        stage.setScene(scene);
+        stage.setResizable(false);
+
+        //dbConn konekcija = new dbConn();
+        dbMetode.kreirajPristupnePodatke();
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
