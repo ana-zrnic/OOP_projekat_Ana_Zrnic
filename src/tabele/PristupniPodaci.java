4426657908e0595ae4f451e74e72e4199902ef47
ana-zrnic <ana.zrnic@student.pmf.unibl.org>
2022-01-06 01:33:10 +0100

    objekti se cuvaju u mapu umjesto u array listu

--- src/tabele/PristupniPodaci.java
+++ src/tabele/PristupniPodaci.java
@@ -1,25 +1,30 @@
 package tabele;
 
-import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Map;
 
 public class PristupniPodaci {
     //private static radSaBazom.dbTest konekcija = new radSaBazom.dbTest();
-    private static ArrayList<PristupniPodaci> sviPristupniPodaci = new ArrayList<>();
+    //private static ArrayList<PristupniPodaci> sviPristupniPodaci = new ArrayList<>();
+    private static Map<Integer, PristupniPodaci> sviPristupniPodaci = new Hashtable<>();
     private final String korisnickoIme, email;  //final?
+    private final int id;
     private String sifra;
 
-    public PristupniPodaci (String korisnickoIme, String email, String sifra){
-        this.korisnickoIme = korisnickoIme; //neka korisnicko ime u obrascu prilikom kreiranja automatski bude ime.prezime
+
+    public PristupniPodaci (String korisnickoIme, String email, String sifra, int id){
+        this.korisnickoIme = korisnickoIme;
         this.email = email;
         this.sifra = sifra;
+        this.id = id;
         if(postojiNalog(this))  //poslati samo atribute ili citav objekat?
             System.out.println("postoji nalog sa ovim podacima"); //exception??
         else {
-            sviPristupniPodaci.add(this);
+            sviPristupniPodaci.put(id, this);
         }
     }
 
-    public static ArrayList<PristupniPodaci> getSviPodaci (){
+    public static Map<Integer, PristupniPodaci> getSviPodaci (){
         return sviPristupniPodaci;
     }
 
@@ -36,7 +41,7 @@ public class PristupniPodaci {
     }
 
     public String toString (){
-        return "korisnik: "+this.korisnickoIme +", "+ this.sifra+", " + this.email;
+        return "[korisnik: "+this.korisnickoIme +", "+ this.sifra+", " + this.email+"]";
     }
 
     public static void kreirajDbObjekte(){
@@ -45,9 +50,11 @@ public class PristupniPodaci {
 
     private boolean postojiNalog(PristupniPodaci podaci){
         if(sviPristupniPodaci!=null)
-            for(PristupniPodaci p : sviPristupniPodaci)
+            for(PristupniPodaci p : sviPristupniPodaci.values())
                 if(p.korisnickoIme.equals(podaci.korisnickoIme) && p.email.equals(podaci.email))
                     return true;
         return false;
     }
+
+
 }
