3070f1f4514cd09cb4e90cc6365b71281f41fc84
ana-zrnic <ana.zrnic@student.pmf.unibl.org>
2022-01-08 19:26:15 +0100

    cuva se u mapu i ima id atribut

--- src/tabele/Skola.java
+++ src/tabele/Skola.java
@@ -1,15 +1,33 @@
 package tabele;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Map;
 
 public class Skola {
-    private static ArrayList<Skola> sveSkole = new ArrayList<>();
+    //private static ArrayList<Skola> sveSkole = new ArrayList<>();
+    private static Map<Integer, Skola> sveSkole = new Hashtable<>();
     private String naziv, grad, mjesto, drzava;
+    private int id;
 
-    public Skola (String naziv, String grad, String mjesto, String drzava){
+    public Skola (String naziv, String grad, String mjesto, String drzava, int id){
         this.drzava = drzava;
         this.grad = grad;
         this.mjesto = mjesto;
         this.naziv = naziv;
+        this.id = id;
+
+        if(skolaPostoji(this))
+            System.out.println("Skola postoji u bazi");
+        else
+            sveSkole.put(id, this);
+    }
+
+    private boolean skolaPostoji(Skola skola){
+        if(sveSkole!=null)
+            for(Skola s : sveSkole.values())
+                if(s.naziv.equals(skola.naziv) && s.mjesto.equals(skola.mjesto) && s.grad.equals(skola.grad) && s.drzava.equals(skola.drzava))
+                    return true;
+        return false;
     }
 }
