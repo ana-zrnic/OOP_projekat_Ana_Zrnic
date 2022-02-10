adfd7d45a79ca8dba6734a6c04ce024cee6625a8
ana-zrnic <ana.zrnic@student.pmf.unibl.org>
2022-02-10 05:54:01 +0100

    seteri i geteri

--- src/tabele/Pitanje.java
+++ src/tabele/Pitanje.java
@@ -1,12 +1,31 @@
 package tabele;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Map;
 
 public class Pitanje {
-    private static ArrayList<Pitanje> svaPitanja = new ArrayList<>();
+    /*private static ArrayList<Pitanje> svaPitanja = new ArrayList<>();*/
+    private static Map<Integer, Pitanje> svaPitanja = new Hashtable<>();
     private String pitanje;
+    private int id;
 
-    public Pitanje (String pitanje){
+    public Pitanje (String pitanje, int id){
         this.pitanje = pitanje;
+        this.id = id;
+
+        svaPitanja.put(id,this);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPitanje() {
+        return pitanje;
+    }
+
+    public static Map<Integer, Pitanje> getSvaPitanja() {
+        return svaPitanja;
     }
 }
